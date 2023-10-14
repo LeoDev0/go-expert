06a50e8648ce308811d7c47d91e242f068487256
Leonardo T. Fernandes <me@leofernandes.dev>
2023-10-14 18:30:34 -0300

    http requests

---
+++ http-requests/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+func main() {
+	startTime := time.Now()
+	request, err := http.Get("http://ident.me")
+	fmt.Printf("Execution time: %s\n", time.Since(startTime))
+	if err != nil {
+		panic(err)
+	}
+	
+	if err != nil {
+		panic(err)
+	}
+	defer request.Body.Close() // Close the connection after the function ends. Defers are executed in LIFO order
+
+	response, err := io.ReadAll(request.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	println("My IP address: " + string(response))
+}
